20a411befd4d60c243e49fcfe9a296a23a4988b2
Bj?rn Ricks <bjoern.ricks@intevation.de>
2014-10-21 13:06:21 +0200

    Link text may contain empty space if it is formatted
    We should not rely on not formatted xml for checking if the link url is in the
    text or in a sub element.

---
+++ py3o/template/tests/test_helpers.py
@@ -0,0 +1,263 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'faide'
+
+import unittest
+import os
+
+import lxml.etree
+import pkg_resources
+
+from pyjon.utils import get_secure_filename
+
+from py3o.template.main import move_siblings, detect_keep_boundary, Template
+
+
+class TestHelpers(unittest.TestCase):
+
+    def tearDown(self):
+        pass
+
+    def setUp(self):
+        template_name = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/py3o_example_template.odt'
+        )
+
+        outname = get_secure_filename()
+
+        self.reference_template = Template(template_name, outname)
+        os.unlink(outname)
+
+    def test_move_1(self):
+        """test that siblings are properly moved without keeping boundaries"""
+        template_one_name = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/move_one.xml'
+        )
+        test_template_one = lxml.etree.parse(template_one_name)
+        start = test_template_one.find('mystruct/start')
+        end = test_template_one.find('mystruct/end')
+        new_ = lxml.etree.Element("finishcontainer")
+
+        move_siblings(start, end, new_)
+
+        result_s = lxml.etree.tostring(
+            test_template_one,
+            pretty_print=True,
+        ).decode('utf-8')
+
+        expected_result = open(
+            pkg_resources.resource_filename(
+                'py3o.template',
+                'tests/templates/move_one_result.xml'
+            )
+        ).read()
+
+        expected_result = expected_result.replace(
+            '\n', '').replace(' ', '').strip()
+
+        result_s = result_s.replace('\n', '').replace(' ', '').strip()
+
+        assert result_s == expected_result
+
+    def test_move_2(self):
+        """start.tail is correctly copied without keeping boundaries"""
+        template_two_name = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/move_two.xml'
+        )
+        test_template_two = lxml.etree.parse(template_two_name)
+        start = test_template_two.find('mystruct/start')
+        end = test_template_two.find('mystruct/end')
+        new_ = lxml.etree.Element('finishcontainer')
+
+        move_siblings(start, end, new_)
+
+        result_s = lxml.etree.tostring(
+            test_template_two,
+            pretty_print=True,
+        ).decode('utf-8')
+
+        print(type(result_s))
+
+        expected_result = open(
+            pkg_resources.resource_filename(
+                'py3o.template',
+                'tests/templates/move_two_result.xml'
+            )
+        ).read()
+
+        expected_result = expected_result.replace(
+            '\n', '').replace(' ', '').strip()
+
+        result_s = result_s.replace('\n', '').replace(' ', '').strip()
+
+        print(result_s)
+
+        assert result_s == expected_result
+
+    def test_move_keep_boundaries(self):
+        """test that siblings are properly moved keeping boundaries"""
+        template_three_name = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/move_three.xml'
+        )
+        test_template_three = lxml.etree.parse(template_three_name)
+        start = test_template_three.find('mystruct/start')
+        end = test_template_three.find('mystruct/end')
+
+        new_ = lxml.etree.Element('finishcontainer')
+
+        move_siblings(
+            start, end, new_,
+            keep_start_boundary=True,
+            keep_end_boundary=True,
+        )
+
+        result_s = lxml.etree.tostring(
+            test_template_three,
+            pretty_print=True,
+        ).decode('utf-8')
+
+        expected_result = open(
+            pkg_resources.resource_filename(
+                'py3o.template',
+                'tests/templates/move_three_result.xml'
+            )
+        ).read()
+
+        expected_result = expected_result.replace(
+            '\n', '').replace(' ', '').strip()
+
+        result_s = result_s.replace('\n', '').replace(' ', '').strip()
+
+        assert result_s == expected_result
+
+    def test_move_2_keep_boundaries(self):
+        """test that start.tail is correctly copied keeping boundaries"""
+        template_four_name = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/move_four.xml'
+        )
+        test_template_four = lxml.etree.parse(template_four_name)
+        start = test_template_four.find('mystruct/start')
+        end = test_template_four.find('mystruct/end')
+
+        new_ = lxml.etree.Element('finishcontainer')
+
+        move_siblings(
+            start, end, new_,
+            keep_start_boundary=True,
+            keep_end_boundary=True,
+        )
+
+        result_s = lxml.etree.tostring(
+            test_template_four,
+            pretty_print=True,
+        ).decode('utf-8')
+
+        print(type(result_s))
+
+        expected_result = open(
+            pkg_resources.resource_filename(
+                'py3o.template',
+                'tests/templates/move_four_result.xml'
+            )
+        ).read()
+
+        expected_result = expected_result.replace(
+            '\n', '').replace(' ', '').strip()
+
+        result_s = result_s.replace('\n', '').replace(' ', '').strip()
+
+        print(result_s)
+
+        assert result_s == expected_result
+
+    def test_get_user_variables(self):
+        source_odt_filename = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/py3o_example_template.odt'
+        )
+        outfilename = get_secure_filename()
+
+        template = Template(source_odt_filename, outfilename)
+
+        user_vars = template.get_user_variables()
+
+        expected_vars = [
+            'py3o.line.val1',
+            'py3o.line.val2',
+            'py3o.line.val3',
+            'py3o.item.Amount',
+            'py3o.item.Currency',
+            'py3o.item.InvoiceRef',
+            'py3o.document.total',
+        ]
+        assert set(user_vars) == set(expected_vars)
+
+    def test_get_user_instructions(self):
+        source_odt_filename = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/py3o_example_template.odt'
+        )
+        outfilename = get_secure_filename()
+
+        template = Template(source_odt_filename, outfilename)
+
+        user_instructions = template.get_user_instructions()
+
+        expected_vars = [
+            'for="line in items"',
+            '/for',
+            'for="item in items"',
+            'if="item.InvoiceRef==\'#1234\'"',
+            '/if',
+            '/for',
+        ]
+        print(user_instructions)
+        assert set(user_instructions) == set(expected_vars)
+
+    def test_detect_boundary_false(self):
+        """boundary detection should say no!!"""
+
+        source_xml_filename = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/keepboundary_detection_false.xml'
+        )
+
+        test_xml = lxml.etree.parse(source_xml_filename)
+        starts, ends = self.reference_template.handle_instructions(
+            [test_xml], self.reference_template.namespaces
+        )
+        for start, base in starts:
+            end = ends[id(start)]
+            keep_start, keep_end = detect_keep_boundary(
+                start, end,  self.reference_template.namespaces
+            )
+            assert keep_start is False
+            assert keep_end is False
+
+    def test_detect_boundary_true(self):
+        """boundary detection should say yes!!"""
+
+        source_xml_filename = pkg_resources.resource_filename(
+            'py3o.template',
+            'tests/templates/keepboundary_detection_true.xml'
+        )
+
+        test_xml = lxml.etree.parse(source_xml_filename)
+        starts, ends = self.reference_template.handle_instructions(
+            [test_xml], self.reference_template.namespaces
+        )
+        for index, (start, base) in enumerate(starts):
+            end = ends[id(start)]
+            keep_start, keep_end = detect_keep_boundary(
+                start, end,  self.reference_template.namespaces
+            )
+            if index == 0:
+                assert keep_start is False
+                assert keep_end is True
+
+            else:
+                assert False, "We should find one single link"
